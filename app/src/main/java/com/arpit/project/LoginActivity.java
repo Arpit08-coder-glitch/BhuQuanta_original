25cbbdbf7b2cf5162a3342aeba7fe2cc306adb10
Arpit08-coder-glitch <arpitsin28@gmail.com>
2025-02-20 12:22:09 +0530

    changing logo

--- app/src/main/java/com/arpit/project/LoginActivity.java
+++ app/src/main/java/com/arpit/project/LoginActivity.java
@@ -8,7 +8,6 @@ import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
